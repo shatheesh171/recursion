7faabd90c9ab3df8fd28ecf459177938f6af8172
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-17 19:04:46 +0530

    recursive factorial

--- factorial.py
+++ factorial.py
@@ -1,1 +1,8 @@
-print("Hello world")
+def factorial(n):
+    assert n>=0 and int(n)==n, 'The number must be positive integer only'
+    if n==0:
+        return 1
+    return n*factorial(n-1)
+
+
+print(factorial(-8))
